d6e24842b757da680f7b25935db5de80c8a55174
allllla2000 <intergalacticwoman@gmail.com>
2022-09-27 10:38:53 +0300

    выполненное ДЗ номер 2.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car("BMW", 4);
+        Truck truck = new Truck("MANN",  8);
+        Bicycle bicycle = new Bicycle("Aist", 2);
+
+        ServiceStation station = new ServiceStation();
+        station.check(bicycle);
+        station.check(car);
+        station.check(truck);
+
     }
 }
